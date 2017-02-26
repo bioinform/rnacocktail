36d7b745232dbf5a636feab8f5855a9d8b8ad30c
Sahraeian <sahraeis@rpbuchop026.cm.cluster>
2017-02-26 12:27:07 -0800

    Fixed docker

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def run_pipeline(args,parser):
 
     mode = args.mode
     create_dirs([args.workdir, args.outdir,os.path.join(args.workdir,"logs")])
-    log_file=os.path.join(args.workdir,"logs","run-sample-%s-mode-%s-%s.log"%(
+    log_file=os.path.join(args.workdir,"logs","run-%s-%s-%s.log"%(
                           ("-".join(args.sample)) if mode in ["diff","all"] else args.sample,
                           mode, time.strftime("%Y%m%d-%H%M%S")))
     FORMAT = '%(levelname)s %(asctime)-15s %(name)-20s %(message)s'
