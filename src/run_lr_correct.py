cae51e188424b8ce0505c887e3ec7aa4aaed7cf9
Sahraeian <sahraeis@lb021login.ib.rsshpc1.sc1.science.roche.com>
2019-11-03 21:18:23 -0800

    fix lordec

--- src/run_lr_correct.py
+++ src/run_lr_correct.py
@@ -49,7 +49,7 @@ def run_lordec(kmer=23,
 
     if not os.path.exists("%s/tmp/"%work_lordec):
         os.mkdir("%s/tmp/"%work_lordec)
-        os.chmod("%s/tmp/"%work_lordec, 777)
+        os.system("chmod -R 777 %s/tmp/"%work_lordec)
 
     msg = "LoRDEC for %s"%sample
     if start<=step:
